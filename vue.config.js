07b19a107ef4d04d864f31cfe447762b4dd30fa1
Xie <Yukai.Xie@lcfuturecenter.com>
2020-12-05 17:13:59 +0800

    test publish path

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,15 @@
 const path = require('path');
-const publicPath = process.env.NODE_ENV === 'development' ? '/' : './';
+// const publicPath = process.env.NODE_ENV === 'development' ? '/' : './';
 
 function resolve(dir) {
     return path.join(__dirname, dir);
 }
 
 module.exports = {
-    publicPath: publicPath,
+    publicPath: './',
     productionSourceMap: false,
 
     chainWebpack: (config) => {
         config.resolve.alias.set('@', resolve('src'));
-    },
-
-    devServer: {
-        port: 8088,
     },
 };
